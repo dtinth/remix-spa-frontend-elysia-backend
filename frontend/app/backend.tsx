0427eeaa8f893f8fa3726efa2a08c75440bef612
dtinth on MBP M1 <dtinth@spacet.me>
2024-02-08 19:52:38 +0700

    link to backend

--- frontend/app/backend.tsx
+++ frontend/app/backend.tsx
@@ -1,4 +1,8 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../../backend/app/app";
 
-export const backend = treaty<App>("http://localhost:41682");
+export const backend = treaty<App>(
+  location.hostname === "localhost"
+    ? "http://localhost:41682"
+    : location.origin.replace(/:\/\/([^.]+)/, "://$1-backend")
+);
