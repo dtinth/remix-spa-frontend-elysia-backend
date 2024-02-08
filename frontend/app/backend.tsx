5d3f5ed621eee38b6424567bcf0f8e6dae203b9f
dtinth on MBP M1 <dtinth@spacet.me>
2024-02-08 19:53:42 +0700

    bruh

--- frontend/app/backend.tsx
+++ frontend/app/backend.tsx
@@ -2,7 +2,7 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "../../backend/app/app";
 
 export const backend = treaty<App>(
-  location.hostname === "localhost"
-    ? "http://localhost:41682"
-    : location.origin.replace(/:\/\/([^.]+)/, "://$1-backend")
+  typeof location !== "undefined" && location.hostname !== "localhost"
+    ? location.origin.replace(/:\/\/([^.]+)/, "://$1-backend")
+    : "http://localhost:41682"
 );
