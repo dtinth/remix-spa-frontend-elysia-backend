666cca561fdaca15a779d3138b4b3c4586425760
Thai Pangsakulyanont <dtinth@spacet.me>
2024-02-09 17:01:24 +0700

    re-enable actions test (#9)

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,8 +5,7 @@ test("shows text from backend", async ({ page }) => {
   await expect(page.locator("body")).toContainText("Message from backend: yo!");
 });
 
-// https://github.com/elysiajs/elysia-cors/issues/47
-test.skip("works with actions", async ({ page }) => {
+test("works with actions", async ({ page }) => {
   await page.goto("/");
   await page.getByPlaceholder("Name").fill("user");
   await page.getByRole("button", { name: "Greet me" }).click();
